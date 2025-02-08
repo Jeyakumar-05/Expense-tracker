6d51fa70ac088e24b7ce23b0012f03c9e4213fa5
Jeyakumar <jeyakumar1442@gmail.com>
2025-02-08 22:32:15 +0530

    made responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = ({ userName }) => {
     <nav>
       <NavLink to="/" aria-label="Go to home">
         <img src={logomark} alt="" height={30} />
-        <span>Spendy</span>
+        <span className="nav-text">Spendy</span>
       </NavLink>
       {userName && (
         <Form
